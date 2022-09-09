42dc7665ad0b44d33e76d6766ee89eac932fe0e6
Paulanthonyrocks <58042843+Paulanthonyrocks@users.noreply.github.com>
2022-09-09 18:59:13 -0500

    Update 7-tebahpla.c

--- 0x01-variables_if_else_while/7-tebahpla.c
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -1,30 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int
-
+/**
+ * main - Entry point
  *
- *
- *  * Return: Always 0 (Success)
- *
- *   */
+ * Return: Always 0 (Success)
+ */
 
-
-
- int main(void)
-
+int main(void)
 {
+	char low;
 
-		int d;
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
 
-
-
-			for (d = '0'; d <= '9'; d++)
-
-						putchar(d);
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
